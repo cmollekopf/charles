3e7f98f36ba1bc0ec2b34827b8c16e263e249763
Christian Mollekopf <chrigi_1@fastmail.fm>
2017-10-30 08:26:25 +0100

    Tarball workTarball workss

--- lib/charles.rb
+++ lib/charles.rb
@@ -4,16 +4,15 @@ require 'pry'
 require_relative 'charles/dav'
 require_relative 'charles/smug'
 require_relative 'charles/commandline'
+require_relative 'charles/fileutils'
 
 
 class Flatpak < Thor
-    extend Commandline
-
     desc "rebuild", "Rebuild flatpak"
     def rebuild(*args)
         say "Rebuilding flatpak"
         Dir.chdir("#{Dir.home}/flatpak/") {
-            run "./rebuild.sh"
+            Commandline.run "./rebuild.sh"
         }
     end
 
@@ -21,7 +20,7 @@ class Flatpak < Thor
     def upload(*args)
         say "Upload flatpak"
         Dir.chdir("#{Dir.home}/flatpak/") {
-            run "./upload.sh"
+            Commandline.run "./upload.sh"
         }
     end
 
@@ -29,48 +28,89 @@ class Flatpak < Thor
     def merge_develop(*args)
         say "Merging develop into kolabnow"
         Dir.chdir("#{Dir.home}/flatpak/tmp/kube") {
-            run "git checkout develop"
-            run "git pull"
-            run "git checkout kolabnow"
-            run "git merge develop -m 'Merged branch develop'"
-            run "git push"
+            Commandline.run "git checkout develop"
+            Commandline.run "git pull"
+            Commandline.run "git checkout kolabnow"
+            Commandline.run "git merge develop -m 'Merged branch develop'"
+            Commandline.run "git push"
         }
     end
 
 end
 
 class Git
-    extend Commandline
-    def self.is_clean?()
-        run "git status" do |output|
-            return output.include?('working directory clean')
+    def self.is_clean?
+        Commandline.run "git status" do |output|
+            return output.include? 'working directory clean'
         end
     end
 
     def self.update(branch)
-        run "git checkout #{branch}"
-        run "git pull"
+        Commandline.run "git checkout #{branch}"
+        Commandline.run "git pull"
     end
 
     def self.merge(source, target, message)
-        run "git checkout #{target}"
-        run "git merge #{source} -m '#{message}'"
+        Commandline.run "git checkout #{target}"
+        Commandline.run "git merge #{source} -m '#{message}'"
     end
 end
 
 class Release < Thor
-    extend Commandline
+    no_commands {
+
+        def parseVersionNumber(s)
+            s.scan(/(\d+)/).flatten.map(&:to_i)
+        end
+
+        def cleanCheckout(branch)
+            unless Git.is_clean?
+                say "Directory is not clean"
+                if yes? "Stash?"
+                    Commandline.run "git stash"
+                else
+                    exit
+                end
+            end
+            Commandline.run "git checkout #{branch}"
+            Commandline.run "git pull"
+        end
+
+        def createReleaseTarball(tag)
+            Commandline.run "git archive --prefix=#{tag}/ HEAD | gzip -c > #{tag}.tar.gz"
+        end
+
+        def tagNewRelease(name)
+            latestTag = Commandline.run "git describe --tags --abbrev=0"
+            latestTagCommit = Commandline.run "git log #{latestTag} -n 1 --pretty=format:'%H'"
+            latestCommit = Commandline.run "git log -n 1 --pretty=format:'%H'"
+            if latestTagCommit == latestCommit
+                say "Release commit is available: " + latestTagCommit
+                return latestTag
+            end
+            Fileutils.replaceInFile("CMakeLists.txt", /VERSION_KOLAB (.*)\)/) { |s| (s.to_i + 1).to_s }
+            latestVersionNumber = parseVersionNumber(latestTag)
+            versionNumber = "#{latestVersionNumber[0]}.#{latestVersionNumber[1]}.#{latestVersionNumber[2]}.#{latestVersionNumber[3] + 1}"
+            tag = "#{name}-#{versionNumber}"
+            Commandline.run "git commit -a -m 'Prepared release of #{tag}'"
+            Commandline.run "git tag -u mollekopf@kolabsys.com -s #{tag} -m 'Release of #{tag}'"
+            return tag
+        end
+
+    }
 
     desc "git_kdepim", ""
     def git_kdepim
-        say "Merging develop into kolabnow"
+        say "Release"
         Dir.chdir("#{Dir.home}/kdebuild/kdepim/source/kdepim") {
-            unless Git.is_clean?
-                say "Directory is not clean"
-                return
-            end
-            # Git.update('develop')
-            # Git.merge('develop', 'kolabnow', 'Merged branch develop')
+            branch = "kolab/integration/4.13.0"
+            name = "kdepim"
+            cleanCheckout(branch)
+            tag = tagNewRelease(name)
+
+            # Commandline.run "git push origin #{branch}:#{branch}"
+            # Commandline.run "git push origin #{tag}:#{tag}"
+            createReleaseTarball(tag)
         }
     end
 end
