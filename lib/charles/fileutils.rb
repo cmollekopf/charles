cd5dba6759e35a92e7c281e68dc8dc78fddd3750
Christian Mollekopf <chrigi_1@fastmail.fm>
2017-10-30 12:50:15 +0100

    Obs release

--- lib/charles/fileutils.rb
+++ lib/charles/fileutils.rb
@@ -6,16 +6,20 @@ module Fileutils
     end
 
     def self.replaceInFile(file, regex, &block)
-        lines = []
-        File.open(file, "r") do |f|
-            lines = f.readlines
-            index = lines.find_index{ |l| l.match?(regex) }
-            line = lines[index]
-            original = line[regex, 1]
-            lines[index] = line.gsub(original, block.call(original))
-        end
+        lines = readlinesFromFile(file)
+        index = lines.find_index{ |l| l.match?(regex) }
+        line = lines[index]
+        original = line[regex, 1]
+        lines[index] = line.gsub(original, block.call(original))
         File.open(file, "w") do |f|
             f.write(lines.join)
+        end
+    end
+
+    def self.prependToFile(file, string)
+        lines = readlinesFromFile(file)
+        File.open(file, "w") do |f|
+            f.write(lines.unshift(string).join)
         end
     end
 end
--- lib/charles/release.rb
+++ lib/charles/release.rb
@@ -41,11 +41,20 @@ class Release < Thor
 
         def createReleaseTarball(tag)
             Commandline.run "git archive --prefix=#{tag}/ HEAD | gzip -c > #{tag}.tar.gz"
+            return "#{tag}.tar.gz"
+        end
+
+        def getLatestTag
+            Commandline.run "git describe --tags --abbrev=0"
+        end
+
+        def commitFromTag(tag)
+            Commandline.run "git log #{tag} -n 1 --pretty=format:'%H'"
         end
 
         def tagNewRelease(name)
-            latestTag = Commandline.run "git describe --tags --abbrev=0"
-            latestTagCommit = Commandline.run "git log #{latestTag} -n 1 --pretty=format:'%H'"
+            latestTag = getLatestTag()
+            latestTagCommit = commitFromTag(latestTag)
             latestCommit = Commandline.run "git log -n 1 --pretty=format:'%H'"
             if latestTagCommit == latestCommit
                 say "Release commit is available: " + latestTagCommit
@@ -58,6 +67,15 @@ class Release < Thor
             Commandline.run "git commit -a -m 'Prepared release of #{tag}'"
             Commandline.run "git tag -u mollekopf@kolabsys.com -s #{tag} -m 'Release of #{tag}'"
             return tag
+        end
+
+        def updateDebianTarball(&block)
+            Commandline.run "tar xzf debian.tar.gz"
+            Dir.chdir("debian") {
+                block.call
+            }
+            Commandline.run "tar czf debian.tar.gz debian"
+            Commandline.run "rm -R debian"
         end
 
     }
@@ -74,6 +92,44 @@ class Release < Thor
             # Commandline.run "git push origin #{branch}:#{branch}"
             # Commandline.run "git push origin #{tag}:#{tag}"
             createReleaseTarball(tag)
+        }
+    end
+
+    desc "obs_kdepim", ""
+    def obs_kdepim
+        name = "kdepim"
+        newVersion = ""
+        Dir.chdir("#{Dir.home}/kdebuild/kdepim/source/#{name}") {
+            latestTag = getLatestTag
+            v = parseVersionNumber(latestTag)
+            newVersion = "#{v[0]}.#{v[1]}.#{v[2]}.#{v[3]}"
+            tarball = createReleaseTarball(latestTag)
+            Commandline.run "mv #{tarball} #{Dir.home}/devel/obs/Kontact:4.13:Development/#{name}/#{tarball.gsub("-", "_").gsub("tar.gz", "orig.tar.gz")}"
+        }
+        Dir.chdir("#{Dir.home}/devel/obs/Kontact:4.13:Development/#{name}") {
+            Commandline.run "osc up"
+            Commandline.run "osc revert ."
+            Fileutils.replaceInFile("kdepim.spec", /define patch_version (.*)$/) { |s| (s.to_i + 1).to_s }
+            #TODO insert lines after %changelog
+            regexp = /really4\.13\.0\.(.*)-0~kolab1$/
+            Fileutils.replaceInFile("kdepim-Ubuntu_14.04.dsc", regexp) { |s| (s.to_i + 1).to_s }
+            Fileutils.replaceInFile("kdepim-Ubuntu_16.04.dsc", regexp) { |s| (s.to_i + 1).to_s }
+
+            updateDebianTarball {
+                Fileutils.prependToFile("changelog", <<~HEREDOC)
+                    kdepim (4:#{newVersion}-0~kolab1) unstable; urgency=medium
+
+                      * New upstream release #{newVersion}
+
+                     -- Christian Mollekopf (Kolab Systems) <mollekopf@kolabsystems.com>  #{DateTime.now.strftime('%a, %d %b %Y %k:%M:%S %z')}
+
+                HEREDOC
+            }
+
+            tarballs = Commandline.run("ls *.tar.gz").split("\n")
+            Commandline.run "osc delete #{tarballs[0]}"
+            Commandline.run "osc add #{tarballs[1]}"
+            Commandline.run "osc ci -m 'New release'"
         }
     end
 end
