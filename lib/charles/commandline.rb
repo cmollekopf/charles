45d55a09214d605b14b88091aed1c3108acd01bb
Christian Mollekopf <chrigi_1@fastmail.fm>
2017-12-07 15:18:26 +0100

    Check the exitcode before calling the callback

--- lib/charles/commandline.rb
+++ lib/charles/commandline.rb
@@ -5,16 +5,16 @@ module Commandline
         output = ""
         exit_status = Open3.popen3(cmd) do |stdin, stdout, stderr, thread|
             output = stdout.read.chomp
-            if block_given?
-                block.call(output)
-            else
-                puts output
-            end
             thread.value
         end
         if exit_status != 0
             puts "Nonzero exit code: ", exit_status, "Command: ", cmd
             raise "Command failed"
+        end
+        if block_given?
+            block.call(output)
+        else
+            puts output
         end
         return output
     end
